1f9f01a35c0f5e996dc54009ff82dc02f9848b46
tuxuser <462620+tuxuser@users.noreply.github.com>
2022-05-30 02:32:33 +0200

    quirks.h: Document each quirk-flag

--- src/quirks.h
+++ src/quirks.h
@@ -3,6 +3,8 @@
 #define HAS_QUIRK(quirk_val, enum_val) ((quirk_val & enum_val) == enum_val)
 
 enum CAPTURE_QUIRKS {
+    // Use DILE_VT_CreateEx instead of DILE_VT_Create in capture_init
     QUIRK_DILE_VT_CREATE_EX = 0x1,
+    // Do not freeze frame before capturing in capture_acquire_frame
     QUIRK_DILE_VT_NO_FREEZE_CAPTURE = 0x2
 };
